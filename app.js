0ba708fef5d45647191d38f5dc053b7d4c1eb809
Timothy Paul Ignacio <timothypaulignacio@gmail.com>
2022-09-20 20:11:52 +0800

    WIP: Add html and add styling

---
+++ app.js
@@ -0,0 +1,6 @@
+const canvasRange = document.getElementById('canvas-range');
+const canvas = document.querySelector('.canvas');
+const body = document.querySelector('body')
+
+console.log(canvasRange.getAttribute('value'));
+console.log(body.style.width);
