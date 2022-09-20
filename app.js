caf1a83bc1c787d77b876259465d431530388bc1
Timothy Paul Ignacio <timothypaulignacio@gmail.com>
2022-09-20 23:44:58 +0800

    WIP: add js

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-const canvasRange = document.getElementById('canvas-range');
-const canvas = document.querySelector('.canvas');
-const body = document.querySelector('body')
 
-console.log(canvasRange.getAttribute('value'));
-console.log(body.style.width);
+let sizeText = document.querySelectorAll('.size-text');
+let range = document.getElementById('canvas-range');
+let canvas = document.querySelector('.canvas');
+
+function changeDisplayTextSize() {
+    range.addEventListener('input', function(e) {
+        for (let i=0; i<sizeText.length; i++) {
+            let canvasSize = e.target.value;
+            sizeText[i].textContent = `${canvasSize}`;
+        }
+    })
+}
+
+function addDivsInCanvas(rangeSelected) {
+    let canvasSizeFormat = Number(getComputedStyle(canvas).width.replace('px', ''));
+    let div = document.createElement('div')
+    range.addEventListener('input', function(e) {
+        changeDisplayTextSize();
+        let canvasSelected = Number(e.target.value);
+        sizePerDiv = canvasSizeFormat/canvasSelected;
+        canvas.append(div)*canvasSelected
+    })
+
+}
+
+addDivsInCanvas()
